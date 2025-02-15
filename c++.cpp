9d7bc9599b28dca5b330d18350bcc76b4f251538
askdca <2121287641@qq.com>
2025-02-15 19:26:44 +0800

    Create c++.cpp

---
+++ c++.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    cout << "Hello, World!" << endl;
+
+    int a = 10;
+    cout << "a = " << a << endl;
+
+    string str = "hello,world";
+    cout << str << endl;
+
+    //数据的输入--cin
+    int b = 0;
+    cout << "请输入b的值" << endl;
+    cin >> b;
+    cout << "b的值是" << b << endl;
+
+    
+
+    return 0;
+
+}
